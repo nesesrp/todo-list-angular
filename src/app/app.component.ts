c14b3e80b7b4bff8d20f0300df38397d6cbad899
nesesrp <nesesarpp@gmail.com>
2026-07-22 10:22:00 +0300

    feat(app): wire todo-form and app shell to render the full todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoFormComponent } from './features/todo/todo-form/todo-form.component';
+import { TodoListComponent } from './features/todo/todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoFormComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/features/todo/todo-form/todo-form.component.ts
+++ src/app/features/todo/todo-form/todo-form.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoService } from '../../../core/services/todo.service';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './todo-form.component.html',
   styleUrl: './todo-form.component.css'
 })
 export class TodoFormComponent {
+  private readonly todoService = inject(TodoService);
 
+  readonly title = signal('');
+
+  submit(): void {
+    this.todoService.add(this.title());
+    this.title.set('');
+  }
 }
