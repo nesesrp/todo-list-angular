d1e72dc64c5c4d68bd3ebde4fb0193bb06c5d8cc
nesesrp <nesesarpp@gmail.com>
2026-07-22 10:19:47 +0300

    feat(todo-list): render todos from TodoService via todo-item

--- src/app/features/todo/todo-list/todo-list.component.ts
+++ src/app/features/todo/todo-list/todo-list.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodoService } from '../../../core/services/todo.service';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [TodoItemComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+  private readonly todoService = inject(TodoService);
 
+  readonly todos = this.todoService.todos;
 }
