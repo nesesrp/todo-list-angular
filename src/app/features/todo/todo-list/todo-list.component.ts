91f68ac6f3505bfbe2016d7fbced6a6c150ce561
nesesrp <nesesarpp@gmail.com>
2026-07-23 15:46:31 +0300

    feat(todo-list): add all/active/completed filter for todos

--- src/app/features/todo/todo-list/todo-list.component.ts
+++ src/app/features/todo/todo-list/todo-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { TodoService } from '../../../core/services/todo.service';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
+
+type Filter = 'all' | 'active' | 'completed';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,5 +13,21 @@ import { TodoItemComponent } from '../todo-item/todo-item.component';
 export class TodoListComponent {
   private readonly todoService = inject(TodoService);
 
-  readonly todos = this.todoService.todos;
+  readonly filter = signal<Filter>('all');
+
+  readonly todos = computed(() => {
+    const todos = this.todoService.todos();
+    switch (this.filter()) {
+      case 'active':
+        return todos.filter(todo => !todo.completed);
+      case 'completed':
+        return todos.filter(todo => todo.completed);
+      default:
+        return todos;
+    }
+  });
+
+  setFilter(filter: Filter): void {
+    this.filter.set(filter);
+  }
 }
